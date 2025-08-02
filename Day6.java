35b5b4d899af1163936c8ffe68e9e9fd08c8155f
Nasir-2006 <n9750998@gmail.com>
2025-08-02 12:14:07 +0500

    Day6 of Java

--- Day6.java
+++ Day6.java
@@ -43,9 +43,46 @@
 
 //     }
 // }
-import java.util.Scanner;
-public class Day6{
-    public static void main(String[] args) {
+// import java.util.Scanner;
+// public class Day6{
+//     public static void main(String[] args) {
+//         for(int i=1;i<=15;i++)
+//         {
+//             for(int j=1;j<=i;j++)
+//             {
+//                 if(i==1||i==15||j==1||j==i)
+//                 {
+//                     System.out.print("#");
+//                 }
+//                 else
+//                 {
+//                     System.out.print(" ");
+//                 }
+            
+                
+//             }System.out.print("\n");
+//         }
+//     }
+// }
+// import java.util.Scanner;
+// public class Day6{
+//     public static void main()
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number:");
+//         int num = sc.nextInt();sc.nextLine();
+//         for(int i=1;i<=num;i++)
+//         {
+//             for(int j=1;j<=num-i;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             for(int k=1;k<=i;k++)
+//             {
+//                 System.out.print("*");
+//             }
+//            System.out.println();
+//         }
         
-    }
-}
+//     }
+// }
