01950cd6ed7e8c951c680f40bf5a339256cbb066
Nasir-2006 <n9750998@gmail.com>
2025-08-03 18:12:49 +0500

    Day7 of java

--- Day7.java
+++ Day7.java
@@ -1,26 +1,45 @@
-import java.util.Scanner;
-public class Day7 {
+// import java.util.Scanner;
+// public class Day7 {
 
-    public static void main(String arg[])
-    {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter rows and Col:");
-        int row = sc.nextInt();sc.nextLine();
-        int col = sc.nextInt();sc.nextLine();
-        for(int i=1;i<=row;i++)
-        {
-            for(int j = 1; j<=i ; j++)
-            {
-                if(j%2==0)
-                {
-                    System.out.print("1");
-                }
-                else
-                {
-                    System.out.print("0");
-                }
-            }
-            System.out.println();
-        }
-    }
-}
+//     public static void main(String arg[])
+//     {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("Enter rows and Col:");
+//         int row = sc.nextInt();sc.nextLine();
+//         int col = sc.nextInt();sc.nextLine();
+//         for(int i=1;i<=row;i++)
+//         {
+//             for(int j = 1; j<=i ; j++)
+//             {
+//                 if(j%2==0)
+//                 {
+//                     System.out.print("1");
+//                 }
+//                 else
+//                 {
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+//-------------------------------------------------------------------Simple Right Angle Triangle----------------------------------------------------------------------
+// import java.util.Scanner;
+// public class Day7{
+//     public static void main(String atd[])
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();sc.nextLine();
+//         for(int i=1;i<=num;i++)
+//         {
+//             for(int j=1;j<=i;j++)
+//             {
+//                 System.out.print("$");
+//             }
+//             System.out.println();
+//         } 
+//     }
+// }
+//-----------------------------------------------------------Hollow Right Angle Triangle------------------------------------------------------------------------------
