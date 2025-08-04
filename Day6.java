e6df0de0ae33685c652a5b52d2b634b27d732f9b
Nasir-2006 <n9750998@gmail.com>
2025-08-04 10:32:43 +0500

    Day8 of Java

--- Day6.java
+++ Day6.java
@@ -111,28 +111,28 @@
 
 //     } 
 // }
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Day6 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        int row = sc.nextInt();
+// public class Day6 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter number of rows: ");
+//         int row = sc.nextInt();
 
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = i; j <= row; j++) {
-                if (i == 1 || i == row || j == i || j == row) {
-                    System.out.print("*");
-                }
-                else {
-                    System.out.print(" ");
-                }
-            }
+//         for (int i = 1; i <= row; i++) {
+//             for (int j = 1; j < i; j++) {
+//                 System.out.print(" ");
+//             }
+//             for (int j = i; j <= row; j++) {
+//                 if (i == 1 || i == row || j == i || j == row) {
+//                     System.out.print("*");
+//                 }
+//                 else {
+//                     System.out.print(" ");
+//                 }
+//             }
 
-            System.out.println();
-        }
-    }
-}
+//             System.out.println();
+//         }
+//     }
+// }
---
+++ Day8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day8
+{
+    public static void main(String atd[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String st = sc.nextLine();
+        System.out.println("You entered"+st);
+        sc.close();
+    }
+}|
