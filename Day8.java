4482b018e8e29391ac4fbcf5c1776a1e1b6751e7
Nasir-2006 <n9750998@gmail.com>
2025-08-05 09:57:54 +0500

    Day8 of Java

--- Day8.java
+++ Day8.java
@@ -1,12 +1,49 @@
-import java.util.Scanner;
-public class Day8
-{
-    public static void main(String atd[])
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string");
-        String st = sc.nextLine();
-        System.out.println("You entered"+st);
-        sc.close();
-    }
-}|
+// import java.util.Scanner;
+// public class Day8
+// {
+//     public static void main(String atd[])
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter string");
+//         String st = sc.nextLine();
+//         System.out.println("You entered "+st.toUpperCase());
+//         System.out.println("The location is :"+st.indexOf("NASIR"));
+//         System.out.println(st.length());
+//         char ch = 'A';
+//         for(ch = 'A';ch<='Z';ch++)
+//         {
+//             if((int)ch%2==0)
+//             System.out.print(ch);
+//             else
+//             System.out.print(0);
+//         }
+//         sc.close();
+//     }
+// }
+// import java.util.Scanner;
+// public class Day8{
+//     public static void main(String atd[])
+//     {
+//         int num;
+//         System.out.println("Enter array elements:");
+//         Scanner sc = new Scanner(System.in);
+//         num = sc.nextInt();sc.nextLine();
+//         int[] arr = new int[num];
+//         for(int i = 0;i<num;i++)
+//         {
+//             arr[i]=sc.nextInt();
+//         }
+//         int smallest = arr[0];
+//         for (int val : arr)
+//         {
+//             if (val < smallest)
+//             {
+//                 smallest = val;
+//             }
+//         }
+//         System.out.println("Smallest number is: " + smallest);
+    
+
+
+//     }
+// }
