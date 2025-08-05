73ab07c720904b85fd5039c6e31d9d9ff507732e
Nasir-2006 <n9750998@gmail.com>
2025-08-05 10:26:52 +0500

    Day9 of java

---
+++ Day9.java
@@ -0,0 +1,33 @@
+// import java.util.Scanner;
+// public class Day9 {
+//     static void Method( String name,int age,char gen,String  num)
+//     {
+//         System.out.print("Nasir "+ name+ " age is : "+ age+",Gender is "+ gen+" Number is :"+num);
+//     }
+//     public static void main()
+//     {
+//         Method("Mehmood",19,'M',"03001481947");
+//     }
+    
+// }
+import java.util.Scanner;
+public class Day9{
+static void check(int age)
+{
+    if(age<18)
+    {
+        System.out.println("Not eligible");
+    }
+    else
+    System.out.println("Eligible");
+}
+public static void main(String atd[])
+{
+    int age;
+    Scanner sc = new Scanner(System.in);
+    System.err.println("Enter age:");
+    age = sc.nextInt();sc.nextLine();
+    check(age);sc.close();
+}
+
+}
